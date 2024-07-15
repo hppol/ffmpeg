86c9fd9685653f538a68674c61c88156470b09dd
hppol <hppol0409@naver.com>
2024-07-15 14:25:44 +0900

    subprocess 추가

--- 1.py
+++ 1.py
@@ -1,5 +1,9 @@
 import pyperclip
+import subprocess
 while True:
     a = input("link: ")
     b = input("name: ")
     pyperclip.copy("ffmpeg -i \"%s\" -c copy %s.ts " %(a,b))
+    subprocess.call('start', shell = True)
+
+
